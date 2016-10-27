9a5cf4757e9861d3100b7adcd62388880f82c239
daril87 <daril.flora@gmail.com>
2016-10-27 12:18:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Dynamic</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="msapplication-tap-highlight" content="no">
+
+  <link rel="icon" type="image/x-icon" href="assets/icon/favicon.ico">
+  <meta name="theme-color" content="#4e8ef7">
+
+  
+
+  <link href="build/main.css" rel="stylesheet">
+
+</head>
+<body>
+
+</body>
+</html>
